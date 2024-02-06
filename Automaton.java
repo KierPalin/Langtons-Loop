408450faf5e62f5d4064b54a90e8d5bef60fd3c5
KierPalin <kierpalin@gmail.com>
2024-02-06 13:15:28 +0000

    Automaton.java: Requires that the number of simulation steps are passed as int argument upon program start. Program now runs for a set number of epochs.

--- Automaton.java
+++ Automaton.java
@@ -4,10 +4,16 @@ import java.util.concurrent.TimeUnit;
 
 
 public class Automaton extends JFrame {
-	private final Grid grid;
 	private final long delayQuantityMS = 125;
 
+	private final Grid grid;
+	private int max_epochs;
+	private int current_epoch;
+
 	public Automaton(int epochs, RuleSet rules) {
+		this.max_epochs = epochs;
+		this.current_epoch = 0;
+
 		this.setBounds(0, 0, 640, 702);
 		this.grid = new Grid(rules, 512, 512);
 		this.getContentPane().add((JPanel) this.grid);
@@ -19,17 +25,18 @@ public class Automaton extends JFrame {
 
 	public void simulate() {
 		try {
-			while (true) {
+			while (this.current_epoch < this.max_epochs) {
 				this.grid.updateGrid();
 				this.getContentPane().paint(this.getGraphics());
 				TimeUnit.MILLISECONDS.sleep(this.delayQuantityMS);
+				this.current_epoch++;
 			}
 		} catch (Exception e) {}
 	}
 
 
 	public static void main(String[] args) {
-		Automaton automaton = new Automaton(400, new LangtonsLoop());
+		Automaton automaton = new Automaton(Integer.parseInt(args[0]), new LangtonsLoop());
 		automaton.simulate();
 	}
 }
